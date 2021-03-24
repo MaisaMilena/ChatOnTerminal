bce73fa1afa1f562f42de9ef97039347f32fcd36
Maisa <maisa.milena@gmail.com>
2021-03-24 11:58:24 -0300

    Add cliConfig

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -1,6 +1,11 @@
 import Events from 'events'
+import CliConfig from './src/cliConfig.js';
 import TerminalController from "./src/terminalController.js";
 
+// console.log('process.argv', process.argv);
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
+console.log('config', config)
 const componentEmitter = new Events();
 
 const controller = new TerminalController();
---
+++ hacker-chat/client/src/cliConfig.js
@@ -0,0 +1,28 @@
+export default class CliConfig {
+  
+  constructor({username, room, hostUri}){
+    this.username = username;
+    this.room = room;
+    const {hostname, port, protocol} = new URL(hostUri)
+    this.host = hostname;
+    this.port = port;
+    this.protocol = protocol.replace(/\W/, '');
+  }
+
+  static parseArguments(commands){
+    const cmd = new Map();
+
+    for(const key in commands){
+      const index = parseInt(key, 10);
+      const command = commands[key];
+
+      const commandPrefix = '--'
+      if(!command.includes(commandPrefix)) continue;
+      cmd.set(
+        command.replace(commandPrefix, ''),
+        commands[index + 1]
+      )
+    }
+    return new CliConfig(Object.fromEntries(cmd));
+  }
+}
