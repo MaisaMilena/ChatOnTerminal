c9269bfb6f09fcf7a316da46cdac040c80b39941
Maisa <maisa.milena@gmail.com>
2021-03-27 21:56:10 -0300

    Remove comments, update package

--- server/src/index.js
+++ server/src/index.js
@@ -5,29 +5,6 @@ import Controller from './controller.js';
 
 const eventEmitter = new Event();
 
-// async function testServer(){
-//   const options = {
-//     port: 9898,
-//     host: 'localhost',
-//     headers: {
-//       Connection: 'Upgrade',
-//       Upgrade: 'websocket'
-//     }
-//   }
-//   const http = await import('http')
-//   const req = http.request(options)
-//   req.end()
-
-//   req.on('upgrade', (res, socket) =>{
-//     socket.on('data', data => {
-//       console.log('client received: ', data.toString())
-//     })
-//     setInterval(() => {
-//       socket.write('Hello!')
-//     }, 500)
-//   })
-// }
-
 const port = process.env.PORT || 9898;
 const socketServer = new SocketServer({ port });
 const server = await socketServer.initialize(eventEmitter);
@@ -39,14 +16,3 @@ eventEmitter.on(
   constants.event.NEW_USER_CONNECTED, 
   controller.onNewConnection.bind(controller)
 )
-
-
-// eventEmitter.on(constants.event.NEW_USER_CONNECTED, (socket) => {
-//   console.log('new connection! ', socket.id)
-//   socket.on('data', data => {
-//     console.log('server received', data.toString())
-//     socket.write('World!')
-//   })
-// })
-
-// await testServer()
